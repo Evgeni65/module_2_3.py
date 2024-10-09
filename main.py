e3018bb492bc60b02e50bd843e390870e09df7f8
Evgeni65 <evgenikorolev56@gmail.com>
2024-10-10 00:47:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+nam = 0
+while True:
+     if (my_list[nam]) > 0:
+         print(my_list[nam])
+     if nam >= (len(my_list) - 1):
+         break
+     else:
+        nam = nam + 1
+     continue
+
+
+
